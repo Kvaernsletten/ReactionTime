dae8387524a0309c951c3e2f1686b6ba92701309
Kvaernsletten <kvaernsletten@gmail.com>
2024-03-26 14:59:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+let app = document.getElementById('app');
+let lampGrid = [];
+let selectedLampIndex;
+let previousLampIndex;
+let isLit;
+
+let startTime = new Date().getTime();
+let finishTime = new Date().getTime();
+let milliseconds = Math.floor(finishTime - startTime);
+let seconds = milliseconds / 1000;
+
+
+createLampGrid();
+lightUp();
+updateView();
+
+function updateView() {
+    app.innerHTML = /*HTML*/ `
+    <div class="timer">Reaction timer</div>
+        <div class="container">
+            ${lampGrid.join('')}
+        </div>
+        <div class="timer">${seconds}</div>
+    `;
+}
+
+function createLampGrid() {
+    for(let i = 0; i < 25; i++){
+        lampGrid.push(`<div class="circle"></div>`);
+    }
+}
+
+function lightUp() {
+
+    selectedLampIndex = Math.floor(Math.random() * 24);
+
+    if(selectedLampIndex != previousLampIndex){
+        finishTime = new Date().getTime();
+        milliseconds = Math.floor(finishTime - startTime);
+        seconds = milliseconds / 1000;
+        lampGrid[previousLampIndex] = `<div class="circle"></div>`;
+        lampGrid[selectedLampIndex] = `<div onclick="lightUp()" class="circle lightOn"></div>`;
+        previousLampIndex = selectedLampIndex;
+    }
+    else{
+        lightUp();
+    }
+    startTime = new Date().getTime();
+    updateView();
+}
